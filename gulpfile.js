248d9797c9d2b4db64e6d8c92f7834cd77c4e8b6
Alex Schweiz <askibinskiy@gmail.com>
2017-05-04 21:39:12 +0300

    Changed structure and added 2 pages

--- gulpfile.js
+++ gulpfile.js
@@ -6,55 +6,55 @@ var minify = require('gulp-minify');
 var watch = require('gulp-watch');
 
 gulp.task('html', function buildHTML() {
-    return gulp.src('app/pug/*.pug')
+    return gulp.src('app/admin/pug/*.pug')
         .pipe(pug({
             pretty: true
         }))
-        .pipe(gulp.dest('public'));
+        .pipe(gulp.dest('public/admin'));
 });
 
 //CSS Tasks
 gulp.task('styles', function buildStyles(){
-    return gulp.src('app/css/*.css')
+    return gulp.src('app/admin/css/*.css')
         .pipe(cleanCSS())
-        .pipe(gulp.dest('public/css'))
+        .pipe(gulp.dest('public/admin/css'))
 });
 
 gulp.task('moveBootstrapCSS', function buildStyles(){
     return gulp.src('node_modules/bootstrap/dist/css/bootstrap.css')
         .pipe(cleanCSS())
-        .pipe(gulp.dest('public/bootstrap/css'))
+        .pipe(gulp.dest('public/admin/bootstrap/css'))
 });
 
 gulp.task('moveSkinsCSS', function buildStyles(){
-    return gulp.src('app/css/skins/*.css')
+    return gulp.src('app/admin/css/skins/*.css')
         .pipe(cleanCSS())
-        .pipe(gulp.dest('public/css/skins'))
+        .pipe(gulp.dest('public/admin/css/skins'))
 });
 
 //JS Tasks
 gulp.task('moveJQuery', function() {
     gulp.src('node_modules/jquery/dist/jquery.js')
         .pipe(minify())
-        .pipe(gulp.dest('public/js/jquery'));
+        .pipe(gulp.dest('public/admin/js/jquery'));
 });
 
 gulp.task('moveBootstrapJS', function() {
     gulp.src('node_modules/bootstrap/dist/js/bootstrap.js')
         .pipe(minify())
-        .pipe(gulp.dest('public/bootstrap/js'));
+        .pipe(gulp.dest('public/admin/bootstrap/js'));
 });
 gulp.task('moveAppJS', function() {
-    gulp.src('app/js/*.js')
+    gulp.src('app/admin/js/*.js')
         .pipe(minify())
-        .pipe(gulp.dest('public/js'));
+        .pipe(gulp.dest('public/admin/js'));
 });
 
 //Image Tasks
 gulp.task('images', function() {
-    return gulp.src('app/img/**/*')
+    return gulp.src('app/admin/img/**/*')
         .pipe(imagemin({optimizationLevel: 5}))
-        .pipe(gulp.dest('public/img'));
+        .pipe(gulp.dest('public/admin/img'));
 });
 
 gulp.task('watch', function() {
